5da60a927dd27fe22a5f704f6fbaa9d984f9394e
Florian <florian.levasseur@hotmail.fr>
2022-04-27 10:16:44 +0200

    Intégration API et recherche en français en Mobile first

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,36 @@
-document.getElementById('searchPkmn').addEventListener('click', function () { //=>
-    axios.get('https://pokeapi.co/api/v2/pokemon/' + document.getElementById('mySearch').value.toLowerCase())
+let myId = "";
+
+async function getData() {
+    myId = "";
+    await axios.get("./assets/js/myPokedex.json")
         .then(function (response) {
-            console.log(response.data);
-            let myData = response.data;
-            document.getElementById('myImg').src = myData.sprites.front_default;
-            document.getElementById('description').innerHTML = `
-                                                                ${myData.name.toUpperCase()}<br><br>
-                                                                Type : <img src="assets/img/${myData.types[0].type.name}.png" class="size"><br>
-                                                                Taille : ${myData.height / 10}m<br>
-                                                                Poids : ${myData.weight / 10}kg`;
+            myId = response.data.pokedex.find(item => item.name.toLowerCase() == document.getElementById('mySearch').value.toLowerCase()).id
         })
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + myId)
+        .then(function (response) {
+            let myData = response.data;
+            let typesTable = myData.types;
+            let myDescription = "";
+            document.getElementById('myImg').src = myData.sprites.front_default;
+            myDescription = `${document.getElementById('mySearch').value.toUpperCase()}<br><br>Type :`;
+            typesTable.forEach(element =>
+                myDescription += ` <img src="assets/img/${element.type.name}.png" class="size"> /`
+            );
+            myDescription = myDescription.slice(0, -1);
+            myDescription += `<br>
+                            Taille : ${myData.height / 10}m<br>
+                            Poids : ${myData.weight / 10}kg`;
+            document.getElementById('description').innerHTML = myDescription;
+        })
+}
+
+document.getElementById('searchPkmn').addEventListener('click', function () {
+    getData();
 })
 
-document.getElementById('mySearch').addEventListener('keyup', function (e) { //=>
+document.getElementById('mySearch').addEventListener('keyup', function (e) {
     if (e.keyCode == 13) {
-        axios.get('https://pokeapi.co/api/v2/pokemon/' + document.getElementById('mySearch').value.toLowerCase())
-            .then(function (response) {
-                console.log(response.data);
-                let myData = response.data;
-                let typesTable = myData.types;
-                let myDescription = "";
-                document.getElementById('myImg').src = myData.sprites.front_default;
-                myDescription = `${myData.name.toUpperCase()}<br><br>Type :`;
-                typesTable.forEach(element =>
-                    myDescription += ` <img src="assets/img/${element.type.name}.png" class="size"> /`
-                );
-                myDescription = myDescription.slice(0, -1);
-                myDescription += `<br>
-                                Taille : ${myData.height / 10}m<br>
-                                Poids : ${myData.weight / 10}kg`;
-                document.getElementById('description').innerHTML = myDescription;
-            })
+        getData();
     }
 
 })
